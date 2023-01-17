e66ba9791b81d83efca2b7db875a9d323c5c0b79
MamaiOksana <jayko.mamayko@gmail.com>
2023-01-17 02:11:14 +0200

    add lesson 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import {Bart} from "./components/SimpsonFamily/bart";
+import {Maggie} from "./components/SimpsonFamily/maggie";
+import {Homer} from "./components/SimpsonFamily/homer";
+import {Marge} from "./components/SimpsonFamily/marge";
+import {Lisa} from "./components/SimpsonFamily/lisa";
 
 function App() {
+
+    let [actors, setActors] = useState([]);
+
+    useEffect(() => {
+       fetch('https://rickandmortyapi.com/api/character')
+            .then(value => value.json())
+            .then(value => {
+                setActors(value.results.slice(0,6));
+
+                console.log(value.results.slice(0,6));
+            });
+    },[]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        {
+            actors.map(value => <div>
+                 {
+                    <div className="Actors">
+                        <h4> {value.id}--{value.name}</h4>
+                        <div><b>status</b> - {value.status}</div>
+                        <div><b>species</b>: {value.species}</div>
+                        <div><b>gender</b>: {value.gender}</div>
+                        <img src={value.image} alt={""}/>
+                    </div>
+                 }</div>)
+        }
+        <div>
+            <Homer/>
+        </div>
+        <div>
+            <Marge/>
+        </div>
+        <div>
+            <Bart/>
+        </div>
+        <div>
+            <Lisa/>
+        </div>
+        <div>
+            <Maggie/>
+        </div>
     </div>
-  );
+  )
 }
 
 export default App;
+
